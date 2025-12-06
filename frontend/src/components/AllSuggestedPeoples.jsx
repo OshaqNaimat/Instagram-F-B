c695168399cec1503b871b4b43da3b004bc284b3
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-06 17:19:15 +0500

    followers suggestions

--- frontend/src/components/AllSuggestedPeoples.jsx
+++ frontend/src/components/AllSuggestedPeoples.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { GoDotFill } from "react-icons/go";
 import Fpeoples from "./Fpeoples";
+import { IoClose } from "react-icons/io5";
 
-const Followers = ({ allSuggestions, setAllSuggestions }) => {
+const Followers = ({ allSuggestions, setAllSuggestion }) => {
   return (
     <>
-      <div className="bg-black/50 min-h-screen w-full fixed top-0 z-100">
+      <div
+        className={`bg-black/50 min-h-screen w-full fixed top-0 z-100 ${
+          allSuggestions ? "" : "hidden"
+        }`}
+      >
         <div
-          className={`${
-            allSuggestions ? "" : "hidden"
-          } container w-[50%] mx-auto shadow-lg overflow-x-hidden rounded-md mt-9 p-3 z-100 overflow-y-scroll h-[600px] bg-white`}
+          className="
+         container w-[50%] mx-auto shadow-lg overflow-x-hidden rounded-md mt-9 p-3 z-100 overflow-y-scroll h-[600px] bg-white"
         >
           <div className="flex my-2 justify-center">
             <h4 className="text-gray-500 font-semibold">Suggested for you</h4>
@@ -33,6 +37,10 @@ const Followers = ({ allSuggestions, setAllSuggestions }) => {
           <Fpeoples />
           <Fpeoples />
         </div>
+        <IoClose
+          onClick={() => setAllSuggestion(false)}
+          className="z-100 text-white text-4xl fixed top-10 right-10 cursor-pointer"
+        />
       </div>
     </>
   );
