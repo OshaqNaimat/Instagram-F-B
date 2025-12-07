dc360db1e0c2789b5da5641b37d467cf514c787b
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-07 13:12:18 +0500

    messages

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -7,16 +7,19 @@ const Messages = () => {
   return (
     <>
       {/* chat section */}
-      <div className="grid grid-cols-10">
+      <div className="grid grid-cols-10 select-none">
         <div className="col-span-3 p-8">
           <div className="flex justify-between items-center">
-            <div className="flex gap-2 items-center justify-center">
+            <div className="flex gap-2 items-center justify-center cursor-pointer">
               <h1 className="font-bold text-2xl ">Username</h1>
               <IoIosArrowDown size={22} />
             </div>
-            <FaPenToSquare size={22} />
+            <FaPenToSquare
+              className="cursor-pointer active:scale-115 transition "
+              size={22}
+            />
           </div>
-          <div className="flex shadow-xl shadow-gray-500 rounded-full justify-center items-center">
+          <div className="flex shadow-xl my-2 shadow-gray-500 rounded-full justify-center items-center">
             <IoSearch size={44} className="  p-2 rounded-e-2xl rounded-full" />
             <input
               type="text"
@@ -26,6 +29,12 @@ const Messages = () => {
               className="w-full p-[0.6rem] outline-0 rounded-s-2xl rounded-full"
             />
           </div>
+        </div>
+        <div className="flex">
+          <img
+            src="https://cdn-icons-png.flaticon.com/256/12225/12225935.png"
+            alt=""
+          />
         </div>
         {/* messages section */}
         <div className="col-span-7">
