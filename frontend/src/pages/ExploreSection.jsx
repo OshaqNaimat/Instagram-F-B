002dec24d56eac52487b9dfe7ce2a2a02f1e9eae
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-12 23:18:11 +0500

    explore

--- frontend/src/pages/ExploreSection.jsx
+++ frontend/src/pages/ExploreSection.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const ExploreSection = () => {
   return (
     <>
-      <div class="grid grid-cols-3 grid-rows-5">
+      {/* very first row */}
+      <div className="grid grid-cols-  grid-rows-5">
         {/* <!-- div1 (default position) --> */}
-        <div class="">
+        <div className="">
           <img
             src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
             alt=""
@@ -13,7 +14,7 @@ const ExploreSection = () => {
         </div>
 
         {/* <!-- div2: grid-column-start:1; grid-row-start:2 --> */}
-        <div class="col-start-1 row-start-2">
+        <div className="col-start-1 row-start-2">
           <img
             src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
             alt=""
@@ -21,7 +22,7 @@ const ExploreSection = () => {
         </div>
 
         {/* <!-- div3: grid-column-start:2; grid-row-start:1 --> */}
-        <div class="col-start-2 row-start-1">
+        <div className="col-start-2 row-start-1">
           <img
             src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
             alt=""
@@ -29,7 +30,7 @@ const ExploreSection = () => {
         </div>
 
         {/* <!-- div4: grid-column-start:2; grid-row-start:2 --> */}
-        <div class="col-start-2 row-start-2">
+        <div className="col-start-2 row-start-2">
           <img
             src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
             alt=""
@@ -37,13 +38,54 @@ const ExploreSection = () => {
         </div>
 
         {/* <!-- div5: column 3, row 1, row span 2 --> */}
-        <div class="col-start-3 row-start-1 row-span-2 ">
+        <div className="col-start-3 row-start-1 row-span-2 ">
           <img
             className="h-full object-cover"
             src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
             alt=""
           />
         </div>
+        {/* <!-- div1 (default position) --> */}
+        <div className="">
+          <img
+            src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
+            alt=""
+          />
+        </div>
+
+        {/* <!-- div2: grid-column-start:1; grid-row-start:2 --> */}
+        <div className="col-start-1 row-start-2">
+          <img
+            src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
+            alt=""
+          />
+        </div>
+
+        {/* <!-- div3: grid-column-start:2; grid-row-start:1 --> */}
+        <div className="col-start-2 row-start-1">
+          <img
+            src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
+            alt=""
+          />
+        </div>
+
+        {/* <!-- div4: grid-column-start:2; grid-row-start:2 --> */}
+        <div className="col-start-2 row-start-2">
+          <img
+            src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
+            alt=""
+          />
+        </div>
+
+        {/* <!-- div5: column 3, row 1, row span 2 --> */}
+        <div className="col-start-3 row-start-1 row-span-2 ">
+          <img
+            className="h-full object-cover"
+            src="https://img.freepik.com/premium-photo/reels-instagram-logo-new-feature-social-media-app-3d-rendering_1379-5024.jpg"
+            alt=""
+          />
+        </div>
+        {/* Second row */}
       </div>
     </>
   );
