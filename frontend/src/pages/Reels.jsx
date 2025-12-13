29fe6565aa7b51166322f770667c8e6520f72ac5
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-13 22:02:45 +0500

    reels

--- frontend/src/pages/Reels.jsx
+++ frontend/src/pages/Reels.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { FaRegComment, FaRegHeart, FaRegBookmark } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
 import { BsThreeDots } from "react-icons/bs";
+import { BiRepost } from "react-icons/bi";
 
 const Reels = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center gap-4">
+    <div className="w-screen h-screen flex items-center justify-center ">
       {/* Image Wrapper */}
-      <div className="relative w-full lg:w-[30%] sm:w-[60%] md:w-[50%]   lg:h-[96vh] h-full">
+      <div className="relative w-full lg:w-[30%] sm:w-[60%] md:w-[50%] lg:h-[96vh] h-full">
         <img
           className="rounded-md object-cover w-full h-full"
           src="https://images.pexels.com/photos/220769/pexels-photo-220769.jpeg"
@@ -17,27 +18,27 @@ const Reels = () => {
         {/* Icons (inside for sm & md) */}
         <div
           className="
-            absolute bottom-8 right-3 sm:bottom-8 sm:-right-20 sm:text-black md:text-black md:bottom-8 md:-right-20
-            flex flex-col gap-7 items-center
+            absolute bottom-8 right-5 sm:bottom-8 sm:-right-15 sm:text-black md:text-black md:bottom-8 md:-right-15
+            flex flex-col gap-5
             text-white
-            lg:text-black lg:-right-20 lg:bottom-8
+            lg:text-black lg:-right-15 lg:bottom-8
           "
         >
-          <div className="cursor-pointer flex flex-col items-center text-center ">
+          <div className="cursor-pointer flex flex-col  ">
             <FaRegHeart size={30} className="hover:scale-105 transition" />
-            <p className="text-xs">Likes</p>
           </div>
 
           <div className="cursor-pointer text-center flex flex-col items-center ">
             <FaRegComment size={30} className="hover:scale-105 transition" />
-            <p className="text-xs">Comments</p>
           </div>
 
           <div className="cursor-pointer text-center flex flex-col items-center ">
             <FiSend size={30} className="hover:scale-105 transition" />
-            <p className="text-xs">Send</p>
           </div>
 
+          <div className="cursor-pointer flex flex-col items-center ">
+            <BiRepost size={30} className="hover:scale-105 transition" />
+          </div>
           <div className="cursor-pointer flex flex-col items-center ">
             <FaRegBookmark size={30} className="hover:scale-105 transition" />
           </div>
