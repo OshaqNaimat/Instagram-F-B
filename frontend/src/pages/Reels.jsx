76e9857bfa26881212cb204619051f27f35fbd18
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-13 19:13:13 +0500

    reels

--- frontend/src/pages/Reels.jsx
+++ frontend/src/pages/Reels.jsx
@@ -7,13 +7,13 @@ import { BsThreeDots } from "react-icons/bs";
 
 const Reels = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center p-1 gap-2">
+    <div className="w-screen h-screen flex items-center justify-center p-1 gap-2 releative">
       <img
         className="rounded-md object-cover w-[30%] h-[96vh]"
         src="https://images.pexels.com/photos/220769/pexels-photo-220769.jpeg?cs=srgb&dl=pexels-pixabay-220769.jpg&fm=jpg"
         alt=""
       />
-      <div className="flex justify-center  flex-col gap-7 mt-20 p-2">
+      <div className="flex justify-center flex-col gap-7 mt-20 p-2 absolute right-[27%] bottom-3 ">
         <div className="cursor-pointer ">
           <FaRegHeart size={30} className="hover:scale-105 transition" />
           <p className="text-sm">Likes</p>
@@ -35,7 +35,7 @@ const Reels = () => {
           <p className="text-sm"></p>
         </div>
         <img
-          className="w-[30px] h-[30px] rounded-md"
+          className="w-[30px] h-[30px] rounded-md cursor-pointer hover:scale-105 transition"
           src="https://images.pexels.com/photos/220769/pexels-photo-220769.jpeg?cs=srgb&dl=pexels-pixabay-220769.jpg&fm=jpg"
           alt=""
         />
