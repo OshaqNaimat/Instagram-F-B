441312d52021b22cfe543a1fb1344d1e3bf736e9
Oshaq Naimat <oshaqnaimat15@gmail.com>
2025-12-29 00:17:41 +0500

    sidebar adding

--- frontend/src/components/IconSidebar.jsx
+++ frontend/src/components/IconSidebar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { BiMoviePlay } from "react-icons/bi";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { FaFacebookMessenger, FaRegHeart } from "react-icons/fa6";
+import { IoIosHome, IoMdSearch } from "react-icons/io";
+import { MdOutlineExplore } from "react-icons/md";
 
 const IconSidebar = () => {
   return (
-    <div>
-      <h1>Icon sidebar</h1>
-    </div>
+    <>
+      <div className="flex flex-col bg-blue-500">
+        <IoIosHome />
+        <IoMdSearch />
+        <MdOutlineExplore />
+        <BiMoviePlay />
+        <FaFacebookMessenger />
+        <FaRegHeart />
+        <FaRegPlusSquare />
+      </div>
+    </>
   );
 };
 
