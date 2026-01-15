14af72e8bcb611bab0eceeb8e03d5726dc555b62
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-15 20:18:57 +0500

    search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,26 +1,30 @@
 import React from "react";
+import { IoClose } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className="">
-      <h3 className="font-bold text-3xl">Search</h3>
+    <div>
+      <div className="fixed left-13  top-0 z-100">
+        <h3 className="font-bold text-3xl">Search</h3>
 
-      <div className="relative">
-        <input
-          type="text"
-          //   name="password"
-          placeholder="Search"
-          //   value={formData.password}
-          //   onChange={handleChange}
-          className="w-full px-3 py-2 pr-12 bg-gray-50 shadow-lg shadow-gray-300 rounded text-sm outline-none"
-        />
+        <div className="relative">
+          <input
+            type="text"
+            //   name="password"
+            placeholder="Search"
+            //   value={formData.password}
+            //   onChange={handleChange}
+            className="w-full px-3 py-2 pr-12 bg-gray-50 shadow-lg shadow-gray-300 rounded text-sm outline-none"
+          />
 
-        <span
-          //   onClick={() => setShow(!show)}
-          className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
-        >
-          {/* {show ? <BsEyeSlash /> : <BsEye />} */}
-        </span>
+          <span
+            //   onClick={() => setShow(!show)}
+            className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
+          >
+            <IoClose />
+            {/* {show ? <BsEyeSlash /> : <BsEye />} */}
+          </span>
+        </div>
       </div>
     </div>
   );
