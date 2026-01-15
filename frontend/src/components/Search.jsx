13c0a77dce821117691a69cb89f5b16ab4d8fb67
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-15 20:57:07 +0500

    search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { useSelector } from "react-redux";
-
-const [search, setSearch] = useState("");
-const [searchedUsers, setSearchUsers] = useState([]);
-const { allUsers } = useSelector((state) => state.auth);
+import { Navigate } from "react-router-dom";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const [searchedUsers, setSearchUsers] = useState([]);
+  const { allUsers } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    let foundData = allUsers.filter((item, index) => {
+      return item.username.toLowerCase().startsWith(search.toLowerCase());
+    });
+
+    if (search) {
+      setSearchUsers(foundData);
+    } else {
+      setSearchUsers([]);
+    }
+  }, [search]);
+
   return (
     <div className="bg-white h-screen p-5 rounded-md w-[30%] fixed left-13 z-100 shadow-xl shadow-gray-300">
       <div className="">
@@ -17,8 +30,8 @@ const Search = () => {
             type="text"
             //   name="password"
             placeholder="Search"
-            //   value={formData.password}
-            //   onChange={handleChange}
+            // value={search}
+            onChange={(e) => setSearch(e.target.value)}
             className="w-full px-3 py-2 pr-12 bg-gray-50 shadow-lg shadow-gray-300 rounded text-sm outline-none"
           />
 
@@ -29,6 +42,49 @@ const Search = () => {
             <IoClose />
             {/* {show ? <BsEyeSlash /> : <BsEye />} */}
           </span>
+          <div
+            onClick={Navigate(`profile/${user_id}`)}
+            className="overflow-y-auto flex-1 pr-1 "
+          >
+            {searchedUsers.map((item) => (
+              <div
+                onClick={() => {
+                  setClickUsers(item),
+                    dispatch(
+                      getMessageData({
+                        sender_id: user?._id,
+                        receiver_id: item?._id,
+                      })
+                    );
+                }}
+                key={item.id}
+                className="flex  items-center gap-2 cursor-pointer hover:bg-gray-200 rounded-md p-2"
+              >
+                <img
+                  className="w-10 h-10 rounded-md"
+                  src="https://cdn-icons-png.flaticon.com/256/12225/12225935.png"
+                  alt=""
+                />
+
+                <div>
+                  {item.username
+                    ?.split(new RegExp(`(${search})`, "gi"))
+                    .map((part, index) =>
+                      part.toLowerCase() === search.toLowerCase() ? (
+                        <span
+                          key={index}
+                          className="bg-green-500 text-white font-semibold px-1 rounded"
+                        >
+                          {part}
+                        </span>
+                      ) : (
+                        <span key={index}>{part}</span>
+                      )
+                    )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
